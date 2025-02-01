e7ece02f6459455bf50cb9ba070949ac4ecf9f1c
Bachidze <bachidze90@gmail.com>
2025-02-01 14:01:15 +0400

    feat(Add):test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 3000; // Use Render's PORT or default to 3000
+const PORT = process.env.PORT || 3000; 
 app.use(cors());
 app.use(express.json());
 
